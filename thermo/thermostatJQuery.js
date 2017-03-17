5d65b5b0b45b12d979e76418306cacc63999eccb
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-03-17 15:47:18 +0000

    WIP added some styling

--- thermo/thermostatJQuery.js
+++ thermo/thermostatJQuery.js
@@ -1,6 +1,19 @@
 
 $( document ).ready(function() {
   thermo = new Thermostat();
+  backgroundChange();
+
+
+  function backgroundChange() {
+    var usage = thermo.currentEnergyUsage()
+    if(usage === 'low-usage') {
+      $('body').css("background-color", "rgba(250, 235, 166, 1.0)")
+    } else if (usage === 'high-usage') {
+      $('body').css("background-color", "rgba(240, 165, 202, 1.0)")
+    } else {
+      $('body').css("background-color", "rgba(114, 208, 166, 1.0)")
+    }
+  }
 
   $ ("#current-temp").text(thermo.getCurrentTemperature());
 
@@ -8,18 +21,21 @@ $( document ).ready(function() {
     thermo.increaseTemperature();
     $ ("#current-temp").text(thermo.getCurrentTemperature());
     $ ("#current-energy-usage").text(thermo.currentEnergyUsage());
+    backgroundChange();
   });
 
   $ ("#change-temp-down").click(function() {
     thermo.decreaseTemperature();
     $ ("#current-temp").text(thermo.getCurrentTemperature());
     $ ("#current-energy-usage").text(thermo.currentEnergyUsage());
+    backgroundChange();
   });
 
   $ ("#change-temp-reset").click(function() {
     thermo.resetTemperature();
     $ ("#current-temp").text(thermo.getCurrentTemperature());
     $ ("#current-energy-usage").text(thermo.currentEnergyUsage());
+    backgroundChange();
   });
 
   $ ("#current-psm").text(function() {
@@ -35,9 +51,5 @@ $( document ).ready(function() {
     thermo.powerSavingModeOff();
     $ ("#current-psm").text("OFF");
   });
-
-  $ ("#current-energy-usage").text(thermo.currentEnergyUsage());
-
-
-
+  
 });
